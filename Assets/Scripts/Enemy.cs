bf702eeb13a14db584ee1813da63dcccc010a159
Tanja Kristan <79107605+TanjaKristan@users.noreply.github.com>
2021-05-29 15:54:02 +0200

    Doggo Run

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,25 +11,40 @@ public class Enemy : MonoBehaviour
     Transform target;
     NavMeshAgent agent;
 
+    Animator anim; 
+
 
     // Start is called before the first frame update
     void Start()
     {
         target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
 
     void Update()
     {
 
+        Movement();
+        GetInput();
+    }
+
+
+    void Movement()
+    {
+
         float distance = Vector3.Distance(target.position, transform.position);
         if (distance <= lookRadius)
         {
+            anim.SetBool("running", true);
+            anim.SetInteger("condition", 1);
             agent.SetDestination(target.position);
+
 
             if (distance <= agent.stoppingDistance)
             {
+
                 FaceTarget();
             }
         }
@@ -40,14 +55,32 @@ public class Enemy : MonoBehaviour
 
     void FaceTarget()
     {
+        anim.SetBool("running", false);
+        anim.SetInteger("condition", 0);
         Vector3 direction = (target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = lookRotation;
+
     }
 
     void OnDrawGizmosSelected()
-        {
-           Gizmos.color = Color.red;
-           Gizmos.DrawWireSphere(transform.position, lookRadius);
-            }
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
+
+    void GetInput()
+    {
+        // if gestenattacke, Gegenattacke
+
+
+    }
+
+    void Attacking()
+    {
+        anim.SetBool("attacking", true);
+        anim.SetInteger("condition", 2);
+
+
+    }
+}
